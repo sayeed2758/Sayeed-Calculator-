f0ea0c06d0b95976d1b82ee96f641bc03f6052d0
sayeed2758 <sayeedbackupmail@gmail.com>
2026-08-19 06:15:21 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,21 @@
-const CACHE="sayeed-pro-v4";const ASSETS=["./","./index.html","./manifest.webmanifest"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(r=>{if(e.request.method==="GET"){const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy))}return r}).catch(()=>caches.match("./index.html")))));
+const CACHE="sayeed-calculator-v7";
+const ASSETS=[
+  "./","./index.html","./manifest.webmanifest",
+  "./assets/css/style.css","./assets/js/app.js","./assets/icons/icon.svg"
+];
+self.addEventListener("install",event=>{
+  event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener("activate",event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener("fetch",event=>{
+  if(event.request.method!=="GET") return;
+  event.respondWith(
+    caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE).then(c=>c.put(event.request,copy)).catch(()=>{});
+      return response;
+    }).catch(()=>caches.match("./index.html")))
+  );
+});
